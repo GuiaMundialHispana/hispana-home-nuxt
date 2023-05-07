17bee4c8eb483d6cda4b4908d4b7079bc9efc276
DulcePC <dulcefrancelipc@gmail.com>
2023-05-06 20:20:53 -0400

    :art: improve fetch in search detail

--- pages/search/[id].vue
+++ pages/search/[id].vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div v-if="property != null">
     <PopulationSearchDetailPropertySlides
       :plan-type="property.plan_id"
@@ -11,12 +11,9 @@
     <PopulationSearchDetailPropertyLoan class="md:px-14 px-4" />
     <OrganismExploreProperties />
   </div>
-  <!-- <div class="property" v-if="property === null">
-    chocolate
-  </div> -->
-</template>
+</template> -->
 
-<script>
+<!-- <script>
 export default {
   data() {
     return {
@@ -42,4 +39,33 @@ export default {
     this.getAdsDetails();
   }
 }
+</script> -->
+
+<template>
+  <div v-if="property">
+    <PopulationSearchDetailPropertySlides
+      :plan-type="property.plan_id"
+      :images="property.property.images"
+    />
+    <PopulationSearchDetailPropertyInformation
+      :property="property.property"
+      :user="property.user"
+    />
+    <PopulationSearchDetailPropertyLoan class="md:px-14 px-4" />
+    <OrganismExploreProperties />
+  </div>
+  <div class="p-16" v-if="pending">
+    <OrganismSkeleton class="w-16 h-5" />
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+const config = useRuntimeConfig();
+
+const { data: property, pending, error} = await useFetch(`advertisements/${route.params.id}`, {
+  method: 'GET',
+  baseURL: config.public.API,
+  transform:(_property) => _property.results
+});
 </script>
