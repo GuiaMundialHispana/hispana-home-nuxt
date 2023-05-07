e085825d59fe4f102358f8e46d988dfee3ca5086
DulcePC <dulcefrancelipc@gmail.com>
2023-05-06 20:32:15 -0400

    :art: improve fetch in advertisement home page

--- components/population/Home/VipProperties.vue
+++ components/population/Home/VipProperties.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <section class="pt-6 md:pt-14 lg:px-16 md:px-6 px-4 mx-auto max-w-[97rem]">
     <div class="flex justify-between border-b border-[#F5F5F5] py-2 mb-6">
       <h2 class="text-[28px] leading-8 text-center md:text-left mx-auto md:mx-0 font-medium">
@@ -60,5 +60,58 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
+.swiper-slide { @apply md:w-max; }
+</style> -->
+
+<template>
+  <section v-if="property">
+    <div class="flex justify-between border-b border-[#F5F5F5] py-2 mb-6">
+      <h2 class="text-[28px] leading-8 text-center md:text-left mx-auto md:mx-0 font-medium">
+        Propiedades VIP
+      </h2>
+      <nav class="hidden md:flex gap-4">
+        <AtomsButtons class="prev" btn-type="btn-icon" btn-style="outline-gray" icon-name="arrows/arrow-left" btn-size="xsmall" :icon-size=15 />
+        <AtomsButtons class="next" btn-type="btn-icon" btn-style="outline-gray" icon-name="arrows/arrow-right" btn-size="xsmall" :icon-size=15 />
+      </nav>
+    </div>
+    <Swiper
+      :modules="[SwiperFreeMode, SwiperNavigation, SwiperAutoplay]"
+      :effect="'fade'"
+      :lazy="true"
+      :space-between="32"
+      slides-per-view="auto"
+      :loop="true"
+      :autoplay="{
+        delay: 4000,
+        disableOnInteraction: true
+      }"
+      :navigation="{
+        nextEl: '.next',
+        prevEl: '.prev'
+      }"
+    >
+      <swiper-slide v-for="plan in property" :key="plan">
+        <MoleculesFeaturedProperties :property-id="plan.id" plantype="vip" :property="plan.property" />
+      </swiper-slide>
+    </Swiper>
+  </section>
+  <!-- TODO Skeleton -->
+  <div v-if="pending">
+    cargando
+  </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig();
+
+const { data: property, pending, error} = await useFetch('advertisements/home?plan=1', {
+  method: 'GET',
+  baseURL: config.public.API,
+  transform:(_property) => _property.results.data
+});
+</script>
+
+<style lang="postcss" scoped>
+section { @apply pt-6 md:pt-14 lg:px-16 md:px-6 px-4 mx-auto max-w-[97rem]; }
 .swiper-slide { @apply md:w-max; }
 </style>
