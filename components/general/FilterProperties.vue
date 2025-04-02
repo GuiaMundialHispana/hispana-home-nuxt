277ace9929e078d8383e7cfec1f9abb2a7b237b6
dulcepc <d.perez@kation.com.do>
2025-04-02 11:07:21 -0400

    :bug: add getCities and states to composable

--- components/general/FilterProperties.vue
+++ components/general/FilterProperties.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="absolute left-0 bottom-[-6%] text-[#232323] z-10">
-    <div class="flex items-center overflow-hidden rounded-lg border-2 border-gray-100 bg-neutral-white text-[#232323] shadow-sm w-fit flex-none filterStatus-tabs-lg">
+    <div class=" filterStatus-tabs-lg">
       <AtomsButtons
         v-for="(btn,i) in types"
         @click="sendPath = btn.getPath, sendType = btn.getType, btnSelected = i"
@@ -23,13 +23,14 @@
             </p>
           </div>
         </button>
-        <OnClickOutside @trigger="toggleList('location')" class="dropdown w-[240px]" v-if="dropdownLists.location">
+        <OnClickOutside @trigger="toggleList('location')" v-if="dropdownLists.location" class="dropdown w-[240px]">
+          <!-- Country -->
           <button class="sector-filter-btn" :class="{'active': dropdownLists.country}" @click="toggleList('country')">
             Pais <AtomsIcon class="text-primary-100" name="arrows/arrow-down" :size=16></AtomsIcon>
           </button>
-          <OnClickOutside @trigger="toggleList('country')" v-if="dropdownLists.country">
+          <OnClickOutside v-if="dropdownLists.country" @trigger="toggleList('country')">
             <div class="dropdown-wrapper scrollbar mt-[5px] min-h-max max-h-[283px]">
-              <label class="checkbox-labels" :for="country.name" v-for="country in countries" :key="country">
+              <label class="checkbox-labels" v-for="country in countries" :key="country" :for="country.name">
                 <input
                   type="radio"
                   class="checkbox"
@@ -42,10 +43,11 @@
               </label>
             </div>
           </OnClickOutside>
+          <!-- States -->
           <button v-if="states.length > 0" class="sector-filter-btn" :class="{'active': dropdownLists.sector}" @click="toggleList('sector')">
             Ciudad <AtomsIcon class="text-primary-100" name="arrows/arrow-down" :size=16 />
           </button>
-          <OnClickOutside @trigger="toggleList('sector')" v-if="dropdownLists.sector">
+          <OnClickOutside v-if="dropdownLists.sector" @trigger="toggleList('sector')">
             <div class="dropdown-wrapper scrollbar mt-[5px] min-h-max max-h-[273px]">
               <label class="checkbox-labels" :for="sector.name" v-for="sector in states" :key="sector">
                 <input
@@ -60,10 +62,11 @@
               </label>
             </div>
           </OnClickOutside>
+          <!-- Citys -->
           <button v-if="cities.length > 0" class="sector-filter-btn" :class="{'active': dropdownLists.city}" @click="toggleList('city')">
             Sector <AtomsIcon class="text-primary-100" name="arrows/arrow-down" :size=16></AtomsIcon>
           </button>
-          <OnClickOutside @trigger="toggleList('city')" v-if="dropdownLists.city" >
+          <OnClickOutside v-if="dropdownLists.city" @trigger="toggleList('city')">
             <div class="dropdown-wrapper scrollbar mt-[5px] min-h-max max-h-[273px]">
               <label class="checkbox-labels" :for="city.name" v-for="city in cities" :key="city">
                 <input
@@ -94,7 +97,7 @@
             </p>
           </div>
         </button>
-        <OnClickOutside @trigger="toggleList('propertyType')" class="absolute top-[95%] w-[288px] h-[273px]" v-if="dropdownLists.propertyType">
+        <OnClickOutside @trigger="toggleList('propertyType')" v-if="dropdownLists.propertyType" class="dropdown w-[288px] h-[273px]">
           <div class="dropdown-wrapper scrollbar mt-[5px] min-h-max max-h-[273px]">
             <label class="checkbox-labels" :for="category.name" v-for="category in categories" :key="category">
               <input
@@ -162,11 +165,8 @@
 
 <script lang="ts" setup>
 import { OnClickOutside } from '@vueuse/components';
-import { ref, reactive, watch, onMounted } from 'vue';
-import { useRoute, useRouter, useRuntimeConfig } from '#imports';
 import MultiRangeSlider from 'multi-range-slider-vue';
 
-const route = useRoute();
 const router = useRouter();
 const config = useRuntimeConfig();
 
@@ -192,25 +192,19 @@ const barMaxValue = ref(10000000);
 const showBarMinValue = ref('0');
 const showBarMaxValue = ref('10,000,000');
 const maxPrice = ref(50000000);
-const countries = ref([]);
+const { countries, getStates, states, getCities, cities } = useGetCountry();
 const country_id = ref(0);
-const cities = ref([]);
+
 const city_id = ref(0);
-const states = ref([]);
 const categories = ref([]);
 const category_id = ref(0);
 const state_id = ref(0);
 const picked = ref('RD');
 const price = ref('');
 const priceRangeSteps = ref(500000);
-const bedroomQuantity = ref(0);
-const bathroomQuantity = ref(0);
-const parkingLotQuantity = ref(0);
-const status = ref('');
 const queryBody = reactive({});
 const sendPath = ref('/resultados?type=All');
 const sendType = ref('All');
-const ready = ref(true);
 
 const UpdateValues = (e: any) => {
   barMinValue.value = e.minValue;
@@ -228,25 +222,6 @@ const toggleList = (list: string) => {
   } else {
     dropdownLists[list] = true;
   }
-};
-
-const getCountries = async () => {
-  const countriesApi = await $fetch(config.public.API + 'generals/countries');
-  countriesApi.results.data.forEach((element: any) => {
-    if (element.id === 63 || element.id === 236) {
-      countries.value.push(element);
-    }
-  });
-};
-
-const getStates = async (country_id: number) => {
-  const statesApi = await $fetch(config.public.API + 'generals/states/' + `${country_id}`);
-  states.value = statesApi.results.data;
-};
-
-const getCities = async (state_id: number) => {
-  const citiesApi = await $fetch(config.public.API + 'generals/cities/' + `${state_id}`);
-  cities.value = citiesApi.results.data;
 };
 
 const searchProperties = async () => {
@@ -307,7 +282,7 @@ watch(category_id, (newCategoryId) => {
 });
 
 onMounted(() => {
-  getCountries();
+  // getCountries();
   getCategories();
   queryBody.type = sendType.value;
   queryBody.price_type = picked.value;
@@ -344,6 +319,7 @@ onMounted(() => {
 /*  */
 
 .filterStatus-tabs-lg {
+  @apply flex items-center overflow-hidden rounded-lg border-2 border-gray-100 bg-neutral-white text-[#232323] shadow-sm w-fit flex-none;
   & .btn { @apply flex items-center text-neutral-black border-none bg-neutral-white relative before:w-0.5 before:h-3/4 before:bg-primary-100 before:block before:absolute before:left-0 before:top-1/2 before:-translate-y-2/4 hover:bg-primary-90 hover:text-neutral-white hover:before:hidden first:before:hidden rounded-none !important;
     &.active {
       @apply bg-primary-100 text-neutral-white before:hidden font-semibold !important;
